6b6109556542066e5bf549c8f990a68e68a5fb5a
Tomás Joaquín Antileo Jara <tomas.antileo1801@alumnos.ubiobio.cl>
2023-03-23 00:05:59 -0300

    configuracion de migracion

--- database/migrations/2023_03_23_011424_create_posts_table.php
+++ database/migrations/2023_03_23_011424_create_posts_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
